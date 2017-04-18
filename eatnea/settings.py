3e554bc3d23a2e3c8e2ed6447982d7953e3de6bc
Ariel Montenegro <montenegroariel@gmail.com>
2017-04-18 08:12:09 -0300

    Se agregan apps al requirements.txt

--- eatnea/settings.py
+++ eatnea/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'eatnea.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'eatnea',
+        'USER': 'eatnea',
+        'PASSWORD': '-[MNZ2-fmw&L34<t',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
