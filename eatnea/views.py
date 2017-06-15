e7eb71ea0f7e54ea463f691e720c1286af7d79f8
Ariel Montenegro <you@example.com>
2017-06-15 15:26:39 -0300

    Se agregan las opciones del menu Nosotros

--- eatnea/views.py
+++ eatnea/views.py
@@ -6,3 +6,15 @@ def home(request):
 
 def nosotros(request):
     return render(request, 'nosotros.html')
+
+def quienessomos(request):
+    return render(request, 'quienes_somos.html')
+
+def equipo(request):
+    return render(request, 'equipo.html')
+
+def logros(request):
+    return render(request, 'logros.html')
+
+def formaparte(request):
+    return render(request, 'forma_parte.html')
