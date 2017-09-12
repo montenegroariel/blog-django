4d9e213a794a8a4863cb1a28b12a7b6416921083
Ariel Montenegro <montenegroariel@gmail.com>
2017-09-12 13:50:06 -0300

    Se modifican las imagenes de las entidades

--- eatnea/views.py
+++ eatnea/views.py
@@ -18,3 +18,20 @@ def logros(request):
 
 def formaparte(request):
     return render(request, 'forma_parte.html')
+
+def acercaeat(request):
+    return render(request, 'acerca_eat.html')
+
+# AREAS
+
+def cienciatecnologia(request):
+    return render(request, 'ciencia_tecnologia.html')
+
+def cooperacioninternacional(request):
+    return render(request, 'cooperacion_internacional.html')
+
+def desarrollolocal(request):
+    return render(request, 'desarrollo_local.html')
+
+def empresasemprendimientos(request):
+    return render(request, 'empresas_emprendimientos.html')
